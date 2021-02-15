c23323da6c1f04261537ab9d1b051b63261e781c
BigLIttleLemur <vipeus@gmail.com>
2021-02-15 21:52:33 +0200

    закончил первый этап

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count',
+};
+
+class SearchBar extends React.Component {
+    renderSortbyOption() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOption(sortByOption);
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        });
+    }
+    
+    render() {
+        return (
+            <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+                <ul>
+                {this.renderSortByOption}
+                </ul>
+            </div>
+            <div className="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+                <a>Let's Go</a>
+            </div>
+        </div>
+        );
+    }
+};
+
+<SearchBar />
+
+export default SearchBar;
