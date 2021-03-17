119ddaae7f09afdf18b9db1e172df7c34a462a31
BigLIttleLemur <vipeus@gmail.com>
2021-03-18 00:30:35 +0200

    закончил

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,11 +7,12 @@ class BusinessList extends React.Component {
         return (
             <div className="BusinessList">
                 {this.props.businesses.map( business => {
-                    return <Business business={business} />
+                    return <Business key={business.id} business={business} />
                 })}
             </div>
         );
     }
 };
+
 
 export default BusinessList;
